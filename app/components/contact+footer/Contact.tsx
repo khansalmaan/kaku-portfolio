0f07be0d3102f79a76d5aac02709298139d7c2dc
khansalmaan <leosalmaan5@gmail.com>
2025-09-09 12:45:27 +0530

    refactor: update Contact component layout and replace form with resume section

--- app/components/contact+footer/Contact.tsx
+++ app/components/contact+footer/Contact.tsx
@@ -47,15 +47,13 @@ export default function Contact() {
 
   return (
     <>
+      {/* Resume Section */}
       <section
-        ref={ref}
-        id="contact"
+        id="resume"
         className={`overflow-y-hidden card mt-12 sm:mt-16 md:mt-[100px] px-6 pb-4 md:pb-10 lg:pb-12 flex flex-col lg:items-center lg:flex-row justify-between rounded-2xl bg-linear-to-r from-[#d9d9d91f] to-[#7373731f]`}
       >
         <div
-          className={` ${
-            syne.className
-          } flex justify-between items-center w-full duration-1000`}
+          className={` ${syne.className} flex justify-between items-center w-full duration-1000`}
         >
           <div className="inline w-full">
             <AnimatedTitle
@@ -63,7 +61,41 @@ export default function Contact() {
               charSpace={"mr-[0.001em]"}
               className="text-xl sm:text-2xl md:text-[32px] lg:text-[40px] font-bold pt-4 md:pt-10 lg:pt-12 "
             >
-              GOT A PROJECT IN MIND?
+              View My Resume
+            </AnimatedTitle>
+          </div>
+          <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
+            <button
+              className={`text-base ml-auto mt-6 lg:ml-0 block sm:hidden lg:block lg:text-2xl font-semibold px-4 py-2 md:px-3 lg:py-4 lg:mt-12 rounded-xl border-2 border-white leading-none ${
+                viewCount <= 1 && "duration-500 delay-[1500ms]"
+              } ${
+                inView
+                  ? " opacity-100 translate-y-0"
+                  : "opacity-0 translate-y-16"
+              }`}
+              data-blobity-radius="12"
+            >
+              RESUME
+            </button>
+          </a>
+        </div>
+      </section>
+
+      <section
+        ref={ref}
+        id="contact"
+        className={`overflow-y-hidden card mt-12 sm:mt-16 md:mt-[100px] px-6 pb-4 md:pb-10 lg:pb-12 flex flex-col lg:items-center lg:flex-row justify-between rounded-2xl bg-linear-to-r from-[#d9d9d91f] to-[#7373731f]`}
+      >
+        <div
+          className={` ${syne.className} flex justify-between items-center w-full duration-1000`}
+        >
+          <div className="inline w-full">
+            <AnimatedTitle
+              wordSpace={"mr-2 md:mr-[12px]"}
+              charSpace={"mr-[0.001em]"}
+              className="text-xl sm:text-2xl md:text-[32px] lg:text-[40px] font-bold pt-4 md:pt-10 lg:pt-12 "
+            >
+              Let&#39;s Get In Touch
             </AnimatedTitle>
             <span
               data-blobity
@@ -79,9 +111,7 @@ export default function Contact() {
             className={`text-base ml-auto mt-6 lg:ml-0 block sm:hidden lg:block lg:text-2xl font-semibold px-4 py-2 md:px-3 lg:py-4 lg:mt-12 rounded-xl border-2 border-white leading-none ${
               viewCount <= 1 && "duration-500 delay-[1500ms]"
             } ${
-              inView
-                ? " opacity-100 translate-y-0"
-                : "opacity-0 translate-y-16"
+              inView ? " opacity-100 translate-y-0" : "opacity-0 translate-y-16"
             }`}
             data-blobity-radius="12"
             onClick={() => {
@@ -93,49 +123,7 @@ export default function Contact() {
         </div>
       </section>
 
-      {/* Resume Section */}
-      <section
-        id="resume"
-        className={`overflow-y-hidden card mt-12 sm:mt-16 md:mt-[100px] px-6 pb-4 md:pb-10 lg:pb-12 flex flex-col lg:items-center lg:flex-row justify-between rounded-2xl bg-linear-to-r from-[#d9d9d91f] to-[#7373731f]`}
-      >
-        <div
-          className={` ${
-            syne.className
-          } flex justify-between items-center w-full duration-1000`}
-        >
-          <div className="inline w-full">
-            <AnimatedTitle
-              wordSpace={"mr-2 md:mr-[12px]"}
-              charSpace={"mr-[0.001em]"}
-              className="text-xl sm:text-2xl md:text-[32px] lg:text-[40px] font-bold pt-4 md:pt-10 lg:pt-12 "
-            >
-              View My Resume
-            </AnimatedTitle>
-          </div>
-          <a
-            href="/resume.pdf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button
-              className={`text-base ml-auto mt-6 lg:ml-0 block sm:hidden lg:block lg:text-2xl font-semibold px-4 py-2 md:px-3 lg:py-4 lg:mt-12 rounded-xl border-2 border-white leading-none ${
-                viewCount <= 1 && "duration-500 delay-[1500ms]"
-              } ${
-                inView
-                  ? " opacity-100 translate-y-0"
-                  : "opacity-0 translate-y-16"
-              }`}
-              data-blobity-radius="12"
-            >
-              Resume
-            </button>
-          </a>
-        </div>
-      </section>
       <ToastContainer />
     </>
   );
 }
-
-
-    // const mailtoLink = `mailto:asmitachandel11@gmail.com?subject=New Contact Request from ${encodeURIComponent(
